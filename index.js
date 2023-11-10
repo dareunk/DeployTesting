b7183c22a69a4ba05e677116ef0c096cefc065cd
eunjin choi <83913407+dareunk@users.noreply.github.com>
2023-11-10 17:35:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 80;
 
 const app = express();
 app.get("/", function(req,res) {
-	res.send("hello world");
+	res.send("hello world. Merged from feature branch");
 });
 app.listen(PORT);
 console.log("Running on the port number:" + PORT);
